f62c6f7a911a7e1cae04c19106efda114e74857c
niteshkumar8848 <niteshkumarlodh@gmail.com>
2026-05-21 20:32:46 +0530

    typescript Animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,3 +157,69 @@ socialLinks.forEach(link => {
     this.classList.add('bounce--replay');
   });
 });
+
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.isDeleting = false;
+        this.typingSpeed = 80; // milliseconds per character
+        this.deletingSpeed = 40; // faster deletion
+        this.delayBeforeDelete = this.period; // pause before deleting
+        this.delayBetweenWords = 600; // pause between words
+        this.tick();
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = this.typingSpeed;
+
+        if (this.isDeleting) { 
+            delta = this.deletingSpeed; 
+        }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            // Finished typing - pause before deleting
+            delta = this.delayBeforeDelete;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            // Finished deleting - move to next word
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = this.delayBetweenWords;
+        }
+
+        setTimeout(function() {
+            that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS for smooth typing effect
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 2px solid var(--orange-soda); padding-right: 2px; animation: blink 0.7s infinite; } @keyframes blink { 0%, 49% { border-right-color: var(--orange-soda); } 50%, 100% { border-right-color: transparent; } }";
+        document.body.appendChild(css);
+    };
